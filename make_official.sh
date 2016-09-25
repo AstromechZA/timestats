33bc48ff452504eab2067b73941bf5cf4ddac401
Ben Meier <benmeier@fastmail.com>
2016-09-25 16:45:22 +0200

    Added nice official build help

--- make_official.sh
+++ make_official.sh
@@ -3,10 +3,10 @@
 set -e
 
 # first build the version string
-VERSION=1.0
+VERSION_NUM=1.0
 
 # add the git commit id and date
-VERSION="$VERSION (commit $(git rev-parse --short HEAD) @ $(git log -1 --date=short --pretty=format:%cd))"
+VERSION="$VERSION_NUM (commit $(git rev-parse --short HEAD) @ $(git log -1 --date=short --pretty=format:%cd))"
 
 PROJECT="timestats"
 
@@ -16,7 +16,7 @@ function buildbinary {
 
     echo "Building official $goos $goarch binary for version '$VERSION'"
 
-    outputfolder="build/${goos}_${goarch}"
+    outputfolder="build/$PROJECT-$VERSION_NUM/${goos}_${goarch}"
     echo "Output Folder $outputfolder"
     mkdir -pv $outputfolder
 
@@ -36,3 +36,16 @@ buildbinary darwin amd64
 
 # build for linux
 buildbinary linux amd64
+
+# zip up
+tar -czvf "$PROJECT-$VERSION_NUM.tar.gz" -C "build" "$PROJECT-$VERSION_NUM"
+
+# output text
+echo ""
+echo "How to release:"
+echo ""
+echo "1. Either tag and push the current commit with version $VERSION_NUM or create a new release on github"
+echo "   git tag 'v$VERSION_NUM' && git push && git push --tags"
+echo "2. Upload the $PROJECT-$VERSION_NUM.tar.gz file as the attached file"
+echo "3. Set the release title to '$PROJECT $VERSION_NUM release'"
+echo "3. Write a brief description if required"
